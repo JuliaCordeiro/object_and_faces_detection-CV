48ae58c1656cf53ff99d98770d22fd70f810b5e3
Júlia Cordeiro <julia.c2001@hotmail.com>
2022-10-22 16:44:00 -0300

    Change images folder path

--- object_and_faces_detection/main.py
+++ object_and_faces_detection/main.py
@@ -1,10 +1,10 @@
 import cv2
 import constants
 
-image = cv2.imread('./images/people/camila_achutti.jpg')
+image = cv2.imread('./object_and_faces_detection/images/people/camila_achutti.jpg')
 
 cv2.rectangle(image, (20, 20), (120, 120), constants.white, 1)
 
-cv2.imshow('Rectangle', image)
+cv2.imshow('Retangle', image)
 
 cv2.waitKey(0)
