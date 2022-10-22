1819b79efb0c562046ad2645ecae9f60f16b89a4
Júlia Cordeiro <julia.c2001@hotmail.com>
2022-10-22 16:38:22 -0300

    Retangle design

--- object_and_faces_detection/main.py
+++ object_and_faces_detection/main.py
@@ -1,5 +1,10 @@
 import cv2
+import constants
 
-print(f'OpenCV version {cv2.__version__}')
+image = cv2.imread('./images/people/camila_achutti.jpg')
+
+cv2.rectangle(image, (20, 20), (120, 120), constants.white, 1)
+
+cv2.imshow('Rectangle', image)
 
 cv2.waitKey(0)
