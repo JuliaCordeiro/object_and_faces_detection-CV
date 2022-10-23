e2901eb9ed7726c36c9458c8a00e9a2879f43f21
Júlia Cordeiro <julia.c2001@hotmail.com>
2022-10-23 11:41:30 -0300

    Change file main to only person detection

--- object_and_faces_detection/people_detection.py
+++ object_and_faces_detection/people_detection.py
@@ -2,7 +2,6 @@ import cv2
 import constants
 
 FACE_HAAR = cv2.CascadeClassifier('./object_and_faces_detection/haar_cascade/frontalface.xml')
-CAT_HAAR = cv2.CascadeClassifier('./object_and_faces_detection/haar_cascade/frontalcatface.xml')
 
 def resize_image(image):
   scale_percent = 30
@@ -28,14 +27,6 @@ def person_detection(image):
     minNeighbors = 4)
   print(faces)
   return faces
-  
-def cat_detection(image):
-  cats = CAT_HAAR.detectMultiScale(
-    image = image, 
-    scaleFactor = 1.1, 
-    minNeighbors = 4)
-  print(cats)
-  return cats
 
 def detection_process(image):
   rgb_image = bgr_to_rgb(image)
@@ -43,16 +34,15 @@ def detection_process(image):
   faces = person_detection(rgb_image)
   image = retangle_design(faces, image)
 
-  cats = cat_detection(rgb_image)
-  image = retangle_design(cats, image)
+  cv2.imshow(f'{str(len(faces))} face(s) founded', image)
 
-  cv2.imshow(f'{str(len(faces))} face(s) and {str(len(cats))} cat(s) founded', image)
-
-person = cv2.imread('./object_and_faces_detection/images/people/steve_jobs.jpg')
+person1 = cv2.imread('./object_and_faces_detection/images/people/bill_gates.jpg')
+person2 = cv2.imread('./object_and_faces_detection/images/people/camila_achutti.jpg')
+person3 = cv2.imread('./object_and_faces_detection/images/people/jeff_bezos.jpg')
+person4 = cv2.imread('./object_and_faces_detection/images/people/steve_jobs.jpg')
 people = cv2.imread('./object_and_faces_detection/images/people/lucifer_cast.jpg')
-cat = cv2.imread('./object_and_faces_detection/images/cats/cat_01.jpg')
 
-detection_process(person)
+detection_process(people)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
