74e280ffa6b933afa719a49dff882916c9fc8b56
Júlia Cordeiro <julia.c2001@hotmail.com>
2022-10-23 11:38:39 -0300

    Remove inaccurate cat detection

--- object_and_faces_detection/main.py
+++ object_and_faces_detection/main.py
@@ -12,48 +12,47 @@ def resize_image(image):
   image_resize = cv2.resize(image, dimension, interpolation = cv2.INTER_AREA)
   return image_resize
 
-def make_image_gray(image):
-  gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-  return gray_image
+def bgr_to_rgb(image):
+  rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+  return rgb_image
 
 def retangle_design(param, image):
-  for(x, y, w, h) in param:
-    cv2.rectangle(image, (x, y), (x + w, y + h), constants.white, 3)
+  for(x, y, width, height) in param:
+    cv2.rectangle(image, (x, y), (x + width, y + height), constants.white, 3)
   return image
 
 def person_detection(image):
   faces = FACE_HAAR.detectMultiScale(
-    image, 
+    image = image, 
     scaleFactor = 1.1, 
-    minNeighbors = 2,
-    minSize = (25, 25))
+    minNeighbors = 4)
+  print(faces)
   return faces
   
 def cat_detection(image):
   cats = CAT_HAAR.detectMultiScale(
-    image, 
+    image = image, 
     scaleFactor = 1.1, 
-    minNeighbors = 3,
-    minSize = (75, 75))
+    minNeighbors = 4)
+  print(cats)
   return cats
 
 def detection_process(image):
-  gray_image = make_image_gray(image)
+  rgb_image = bgr_to_rgb(image)
 
-  faces = person_detection(gray_image)
+  faces = person_detection(rgb_image)
   image = retangle_design(faces, image)
 
-  cats = cat_detection(gray_image)
+  cats = cat_detection(rgb_image)
   image = retangle_design(cats, image)
 
-  cv2.imshow(f'{str(len(faces))} face(s) and {str(len(cats))} cats(s) founded', image)
+  cv2.imshow(f'{str(len(faces))} face(s) and {str(len(cats))} cat(s) founded', image)
 
 person = cv2.imread('./object_and_faces_detection/images/people/steve_jobs.jpg')
 people = cv2.imread('./object_and_faces_detection/images/people/lucifer_cast.jpg')
-cat = cv2.imread('./object_and_faces_detection/images/cats/cat_04.jpg')
-person_and_cat = cv2.imread('./object_and_faces_detection/images/person_with_cat.jpg')
+cat = cv2.imread('./object_and_faces_detection/images/cats/cat_01.jpg')
 
-detection_process(cat)
+detection_process(person)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
